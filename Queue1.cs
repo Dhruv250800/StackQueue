6eea379af4fdbbb0d7204f0b09030cadd9a8ea45
DhruvGour <dhruv250800@gmail.com>
2023-06-04 23:03:20 +0530

    [DhruvGour]-adding UC4-Queue3

---
+++ Queue1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Queue1
+    {
+        private Node? front;
+        private Node? rear;
+
+        public int Dequeue()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int data = front.Data;
+            front = front.Next;
+
+            if (front == null)
+            {
+                rear = null;
+            }
+
+            return data;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+    }
+}
