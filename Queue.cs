4451965397e850c72bca424da0a30c02a8b2c803
DhruvGour <dhruv250800@gmail.com>
2023-06-04 22:56:47 +0530

    [DhruvGour]-adding UC3-Queue1

---
+++ Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Queue
+    {
+        private Node? front;
+        private Node? rear;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (rear == null)
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = newNode;
+            }
+        }
+    }
+}
